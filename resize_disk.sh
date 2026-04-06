0aaeda2caf34e1eb3dbc5e02ddc48a9ac584a12e
Tirumala Gudipati <tirugudi@amazon.com>
2026-04-06 09:56:24 +0530

    terraform

--- resize_disk.sh
+++ resize_disk.sh
@@ -1,5 +1,5 @@
 sudo growpart /dev/nvme0n1 4
-sudo lvextend -L +20G /dev/RootVG/rootVol
-sudo lvextend -L +41G /dev/RootVG/varVol
+sudo lvextend -L +15G /dev/RootVG/rootVol
+sudo lvextend -L +30G /dev/RootVG/varVol
 sudo xfs_growfs /
 sudo xfs_growfs /var
