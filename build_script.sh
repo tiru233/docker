cf2982209e27b36e789e690493bf4842cffdb257
Tirumala Gudipati <tirugudi@amazon.com>
2026-04-06 09:40:13 +0530

    terraform

--- build_script.sh
+++ build_script.sh
@@ -13,8 +13,8 @@ docker run -d -p 80:80 --network roboshop --name frontend frontend:1.0.0
 cd ..
 cd user/
 docker build -t user:1.0.0 .
-docker run -d -p 80:80 --network roboshop --name user user:1.0.0
+docker run -d  --network roboshop --name user user:1.0.0
 cd ..
 cd cart/
 docker build -t cart:1.0.0 .
-docker run -d -p 80:80 --network roboshop --name cart cart:1.0.0
+docker run -d --network roboshop --name cart cart:1.0.0
